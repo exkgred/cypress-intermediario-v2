9a856b4d0562ae419659b59d9e52212a6f8f0bc2
exkgred <jooaxe@gmail.com>
2024-01-26 16:48:47 -0300

    aula 6

--- cypress/e2e/gui/createIssue.cy.js
+++ cypress/e2e/gui/createIssue.cy.js
@@ -10,8 +10,9 @@ describe('Create Issue', () => {
         }
     }
     beforeEach(()=>{
+        cy.api_deleteProjects()
         cy.login()
-        cy.gui_createProject(issue.project)
+        cy.api_createProject(issue.project)
 
     })
     it('successfully', ()=>{
